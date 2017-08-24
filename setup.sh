b921bd79afebca923149fed6eb7e6fe1ead7cd11
Marcos Garcia de Lima <marckfree@gmail.com>
2017-08-24 19:16:03 -0300

    Fix variable into string

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ download_hugo() {
 	version="0.26";
 	platform="macOS-64bit";
 	format="tar.gz";
-	url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_${platform}.$format"
+	url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_${platform}.${format}"
 
 	wget $url;
 	tar xzf *.$format;
