eb6b3740cf96b8c4e9945d2ffbe968a67f777db1
Marcos Garcia de Lima <marckfree@gmail.com>
2017-08-24 19:07:49 -0300

    First commit - add setup to Hugo

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# see naming convention in repo https://github.com/gohugoio/hugo/releases
+
+download_hugo() {
+	version="0.26";
+	platform="macOS-64bit";
+	format="tar.gz";
+	url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_${platform}.$format"
+
+	wget $url;
+	tar xzf *.$format;
+	rm -rf README.md LICENSE.md *.$format;
+}
+
+initializer() {
+  download_hugo;
+}
+
+initializer;
