3cd6f36972598850d93679f3c52f9a8c3d96a8ca
Marcos Garcia de Lima <marckfree@gmail.com>
2017-08-24 19:16:54 -0300

    Add commentary

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# see naming convention in repo https://github.com/gohugoio/hugo/releases
 
 download_hugo() {
+	# see naming convention in repo https://github.com/gohugoio/hugo/releases
 	version="0.26";
 	platform="macOS-64bit";
 	format="tar.gz";
+
 	url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_${platform}.${format}"
 
 	wget $url;
