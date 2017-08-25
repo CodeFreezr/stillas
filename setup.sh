3c1a31e14665a4e92f0d7ea2f0c15d834df3058f
Marcos Garcia de Lima <marckfree@gmail.com>
2017-08-25 14:19:22 -0300

    Add autoinstall, npm command to setup and metadata

--- setup.sh
+++ setup.sh
@@ -8,9 +8,14 @@ download_hugo() {
 
 	url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_${platform}.${format}"
 
+	# install Hugo
 	wget $url;
 	tar xzf *.$format;
 	rm -rf README.md LICENSE.md *.$format;
+
+	# create a new site and basic theme
+	./hugo new site site;
+	cd site && .././hugo new theme new-theme;
 }
 
 initializer() {
